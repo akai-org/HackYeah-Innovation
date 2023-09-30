409432dc4ce6b76753997c14f7c9450913e22455
Alan Grądecki <alangradecki@wp.pl>
2023-09-30 20:46:23 +0200

    starting page

--- frontend/HackYeah-Kolobrzeg/src/App.tsx
+++ frontend/HackYeah-Kolobrzeg/src/App.tsx
@@ -1,7 +1,101 @@
-import "./App.css";
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBIcon
+} from "mdb-react-ui-kit";
+
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function App() {
-  return <>HackYeah-Kolobrzeg</>;
+  return (
+    <MDBContainer fluid>
+      <MDBRow className="d-flex justify-content-center align-items-center h-100">
+        <MDBCol col="12">
+          <MDBCard
+            className="bg-white text-dark my-5 mx-auto"
+            style={{ borderRadius: "1rem", maxWidth: "400px" }}
+          >
+            <MDBCardBody className="p-5 d-flex flex-column align-items-center mx-auto w-100">
+              <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
+              <p className="text-dark-50 mb-5">
+                Please enter your login and password!
+              </p>
+
+              <MDBInput
+                wrapperClass="mb-4 mx-5 w-100"
+                labelClass="text-dark"
+                label="Email address"
+                id="formControlLg"
+                type="email"
+                size="lg"
+              />
+              <MDBInput
+                wrapperClass="mb-4 mx-5 w-100"
+                labelClass="text-dark"
+                label="Password"
+                id="formControlLg"
+                type="password"
+                size="lg"
+              />
+
+              <p className="small mb-3 pb-lg-2">
+                <a class="text-dark-50" href="#!">
+                  Forgot password?
+                </a>
+              </p>
+              <MDBBtn outline className="mx-2 px-5" color="white" size="lg">
+                Login
+              </MDBBtn>
+
+              <div className="d-flex flex-row mt-3 mb-5">
+                <MDBBtn
+                  tag="a"
+                  color="none"
+                  className="m-3"
+                  style={{ color: "white" }}
+                >
+                  <MDBIcon fab icon="facebook-f" size="lg" />
+                </MDBBtn>
+
+                <MDBBtn
+                  tag="a"
+                  color="none"
+                  className="m-3"
+                  style={{ color: "white" }}
+                >
+                  <MDBIcon fab icon="twitter" size="lg" />
+                </MDBBtn>
+
+                <MDBBtn
+                  tag="a"
+                  color="none"
+                  className="m-3"
+                  style={{ color: "white" }}
+                >
+                  <MDBIcon fab icon="google" size="lg" />
+                </MDBBtn>
+              </div>
+
+              <div>
+                <p className="mb-0">
+                  Don't have an account?{" "}
+                  <a href="#!" class="text-dark-50 fw-bold">
+                    Sign Up
+                  </a>
+                </p>
+              </div>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
 }
 
 export default App;
