225d7accb325e89a4d9c46b77673227e093b3e05
dominikkm <dominik.malkowiak@gmail.com>
2023-10-01 11:12:14 +0200

    feat: rating system

--- backend/db/migrations/003.sql
+++ backend/db/migrations/003.sql
@@ -3,11 +3,21 @@ CREATE TABLE IF NOT EXISTS university_type (
   name VARCHAR(127) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS score (
+  score_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  a INT NOT NULL,
+  b INT NOT NULL,
+  c INT NOT NULL,
+  d INT NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS university (
   university_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   type_pk INT,
-  FOREIGN KEY (type_pk) REFERENCES university_type(university_type_id)
+  score_pk INT,
+  FOREIGN KEY (type_pk) REFERENCES university_type(university_type_id),
+  FOREIGN KEY (score_pk) REFERENCES score(score_id)
 );
 
 CREATE TABLE IF NOT EXISTS major (
@@ -22,6 +32,13 @@ CREATE TABLE IF NOT EXISTS university_major (
   FOREIGN KEY (university_pk) REFERENCES university(university_id),
   FOREIGN KEY (major_pk) REFERENCES major(major_id)
 );
+
+INSERT INTO score (score_id, a, b, c, d) VALUES
+(1, 100, 50, 25, -8),
+(2, 20, 30, 60, 90),
+(3, 0, 10, 30, 100),
+(4, 30, 32, -3, 15),
+(5, 60, 34, 23, -10);
 
 INSERT INTO university_type (university_type_id, name) VALUES
 (
@@ -49,102 +66,126 @@ INSERT INTO university_type (university_type_id, name) VALUES
   "Uczelnia wychowania fizycznego"
 );
 
-INSERT INTO university (name, type_pk) VALUES
+INSERT INTO university (name, type_pk, score_pk) VALUES
 (
   "Uniwersytet Warszawski",
-  2
+  2,
+  1
 ),
 (
   "Uniwersytet w Białymstoku",
-  2
+  2,
+  3
 ),
 (
   "Uniwersytet Gdański",
-  2
+  2,
+  5
 ),
 (
   "Uniwersytet im. Adama Mickiewicza w Poznaniu",
-  2
+  2,
+  1
 ),
 (
   "Uniwersytet Jagielloński w Krakowie",
-  2
+  2,
+  4
 ),
 (
   "Uniwersytet Marii Curie-Skłodowskiej w Lublinie",
-  2
+  2,
+  3
 ),
 (
   "Uniwersytet Kardynała Stefana Wyszyńskiego w Warszawie",
+  2,
   2
 ),
 (
   "Uniwersytet Kazimierza Wielkiego w Bydgoszczy",
-  2
+  2,
+  1
 ),
 (
   "Politechnika Poznańska",
-  1
+  1,
+  5
 ),
 (
   "Zachodniopomorski Uniwersytet Technologiczny w Szczecinie",
-  1
+  1,
+  5
 ),
 (
   "Akademia Górniczo-Hutnicza im. Stanisława Staszica w Krakowie",
-  1
+  1,
+  4
 ),
 (
   "Politechnika Częstochowska",
-  1
+  1,
+  3
 ),
 (
   "Uniwersytet Ekonomiczny w Katowicach",
-  3
+  3,
+  1
 ),
 (
   "Uniwersytet Ekonomiczny w Krakowie",
-  3
+  3,
+  2
 ),
 (
   "Uniwersytet Ekonomiczny w Poznaniu",
+  3,
   3
 ),
 (
   "Akademia Pedagogiki Specjalnej im. Marii Grzegorzewskiej (Warszawa)",
-  4
+  4,
+  3
 ),
 (
   "Uniwersytet Pedagogiczny im. Komisji Edukacji Narodowej w Krakowie",
-  4
+  4,
+  5
 ),
 (
   "Uniwersytet Przyrodniczo-Humanistyczny w Siedlcach",
-  4
+  4,
+  3
 ),
 (
   "Uniwersytet Rolniczy im. Hugona Kołłątaja w Krakowie",
-  5
+  5,
+  2
 ),
 (
   "Szkoła Główna Gospodarstwa Wiejskiego w Warszawie",
-  5
+  5,
+  4
 ),
 (
   "Uniwersytet Przyrodniczy w Poznaniu",
-  5
+  5,
+  1
 ),
 (
   "Akademia Wychowania Fizycznego i Sportu im. Jędrzeja Śniadeckiego w Gdańsku",
-  6
+  6,
+  5
 ),
 (
   "Akademia Wychowania Fizycznego im. Polskich Olimpijczyków we Wrocławiu",
-  6
+  6,
+  3
 ),
 (
   "Akademia Wychowania Fizycznego im. Eugeniusza Piaseckiego w Poznaniu",
-  6
+  6,
+  1
 );
 
 INSERT INTO major (name) VALUES
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,12 +3,14 @@ import http from "node:http";
 import path from "node:path";
 
 import express from "express";
+import bodyParser from 'body-parser';
 import OpenAI from "openai";
 
 import {test} from "./controllers/api/test";
 import {login} from "./controllers/api/login";
 import {questions_get} from "./controllers/api/questions";
 import {uni_get} from './controllers/api/uni';
+import {bestUni_post} from './controllers/api/best_uni';
 import {Database} from "./classes/Database";
 
 const PORT = 8080;
@@ -31,11 +33,13 @@ const app = express();
   await database.migrate();
 
   app.use(express.static(path.join(__dirname, '..', '../frontend/HackYeah-Kolobrzeg')));
+  app.use(bodyParser.json());
   app.use(test);
 
   app.get('/api/questions', questions_get);
   app.get('/api/uni', uni_get);
 
+  app.post('/api/bestUni', bestUni_post);
   app.post('/login', login);
 
 
